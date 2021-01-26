08177090711bc7748cd7407e114cef808df1fb24
Camila Pelegrina <camila.pelegrina@usp.br>
2021-01-25 22:10:02 -0300

    Login Layout

---
+++ app/src/main/java/com/example/gamesapp/LoginActivity.kt
@@ -0,0 +1,34 @@
+package com.example.gamesapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme)
+        supportActionBar?.hide()
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        findViewById<Button>(R.id.btnLogin).setOnClickListener {
+            callMain()
+        }
+
+       // findViewById<TextView>(R.id.txtRegister).setOnClickListener {
+          //  callCadastro()
+       // }
+    }
+
+    private fun callMain(){
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+
+    //private fun callCadastro(){
+       // val intent = Intent(this, RegisterActivity::class.java)
+      //  startActivity(intent)
+    //}
+}
