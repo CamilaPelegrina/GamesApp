efd7521416c02d3e5010f86999acd1f40395ac9d
Camila Pelegrina <camila.pelegrina@usp.br>
2021-01-29 00:25:13 -0300

    Firebase Storage

--- app/src/main/java/com/example/gamesapp/ui/RegisterGameActivity.kt
+++ app/src/main/java/com/example/gamesapp/ui/RegisterGameActivity.kt
@@ -1,13 +1,70 @@
 package com.example.gamesapp.ui
 
+import android.app.AlertDialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.ImageView
+import android.widget.Toast
 import com.example.gamesapp.R
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import com.squareup.picasso.Picasso
+import dmax.dialog.SpotsDialog
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.floatingActionButton
+import kotlinx.android.synthetic.main.activity_register_game.*
 
 class RegisterGameActivity : AppCompatActivity() {
+    lateinit var alertDialog: AlertDialog
+    lateinit var storageReference: StorageReference
+    private val CODE_IMG = 1000
+
     override fun onCreate(savedInstanceState: Bundle?) {
         supportActionBar?.hide()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register_game)
+
+        config()
+
+        imageView.setOnClickListener{
+            setIntent()
+        }
+    }
+
+    fun setIntent(){
+        val intent = Intent()
+        intent.type = "image/"
+        intent.action = Intent.ACTION_GET_CONTENT
+        startActivityForResult(Intent.createChooser(intent, "capture image"), CODE_IMG)
+    }
+
+    fun config() {
+        alertDialog = SpotsDialog.Builder().setContext(this).build()
+        storageReference = FirebaseStorage.getInstance().getReference("prod_img")
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == CODE_IMG){
+            val uploadTask = storageReference.putFile(data!!.data!!)
+            val task = uploadTask.continueWithTask { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(this, "Receiving", Toast.LENGTH_SHORT).show()
+                }
+                storageReference!!.downloadUrl
+            }.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val downloadUri = task.result
+                    val url = downloadUri!!.toString()
+                        .substring(0, downloadUri.toString().indexOf("&token"))
+                    Log.i("Reference URL: ", url)
+                    alertDialog.dismiss()
+                    Picasso.get().load(url).into(imageView)
+                }
+            }
+        }
     }
 }
